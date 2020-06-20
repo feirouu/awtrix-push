6c3f5be84d9321f07f96b2357878a3d838a1e9e7
Junyu <me@junyu.dev>
2020-06-20 21:59:23 +0800

    change icon type

--- parse/__init__.py
+++ parse/__init__.py
@@ -12,7 +12,16 @@ def vultr(config):
     push_data = {
         "ID": config["id"],
         "text": f'{remaining}G',
-        "icon": [0, 0, 0, 0, 0, 0, 0, 0, 0, 65535, 65535, 0, 669, 669, 0, 0, 0, 5468, 5468, 0, 669, 669, 669, 0, 0, 669, 669, 0, 0, 669, 669, 0, 0, 669, 669, 669, 0, 0, 0, 0, 0, 0, 669, 669, 669, 0, 0, 0, 0, 0, 0, 669, 669, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        "icon": [
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 65535, 65535, 0, 669, 669, 0, 0,
+            0, 5468, 5468, 0, 669, 669, 669, 0,
+            0, 669, 669, 0, 0, 669, 669, 0,
+            0, 669, 669, 669, 0, 0, 0, 0,
+            0, 0, 669, 669, 669, 0, 0, 0,
+            0, 0, 0, 669, 669, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0
+        ]
     }
     awtrix_r = requests.post(config["push_url"], json=push_data)
     if awtrix_r.status_code != requests.codes.ok:
@@ -28,7 +37,7 @@ def bilibili(config):
     push_data = {
         "ID": config["id"],
         "text": resp_data["data"]["follower"],
-        "icon": config["icon"]
+        "icon": int(config["icon"])
     }
     awtrix_r = requests.post(config["push_url"], json=push_data)
     if awtrix_r.status_code != requests.codes.ok:
